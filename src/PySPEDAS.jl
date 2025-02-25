5fc260316e265cea5fa2e91c5083c3b56cbf22d0
Beforerr <zzj956959688@gmail.com>
2025-02-25 10:20:37 -0800

    feat: initial working demo

--- src/PySPEDAS.jl
+++ src/PySPEDAS.jl
@@ -2,9 +2,38 @@ module PySPEDAS
 
 using PythonCall
 
-export tplot, mms
+export pyspedas
+export mms, themis
+export tplot
 
-tplot(args...) = PythonCall.pyimport("pyspedas").tplot(args...)
-mms = PythonCall.pyimport("pyspedas").mms
+const pyspedas = PythonCall.pynew()
+const mms = PythonCall.pynew()
+const themis = PythonCall.pynew()
+
+function __init__()
+    PythonCall.pycopy!(pyspedas, pyimport("pyspedas"))
+    PythonCall.pycopy!(mms, pyimport("pyspedas.projects.mms"))
+    PythonCall.pycopy!(themis, pyimport("pyspedas.projects.themis"))
+end
+
+tplot(args...) = @pyconst(pyspedas.tplot)(args...)
+
+"""Load and plot THEMIS FGM data"""
+function themis_demo(; trange=["2007-03-23", "2007-03-24"])
+    # Load THEMIS FGM data for probe A
+    fgm_vars = themis.fgm(probe='a', trange=trange)
+    # Print the list of tplot variables just loaded
+    println(fgm_vars)
+    # Plot the 'tha_fgl_dsl' variable
+    tplot("tha_fgl_dsl")
+end
+
+function demo(; trange=["2020-04-20/06:00", "2020-04-20/08:00"])
+    pyspedas.projects.solo.mag(; trange, time_clip=true)
+    pyspedas.projects.psp.fields(; trange, time_clip=true)
+    pyspedas.projects.mms.fgm(; trange, time_clip=true, probe=2)
+    pyspedas.projects.themis.fgm(; trange, time_clip=true, probe='d')
+    tplot(["B_RTN", "psp_fld_l2_mag_RTN", "mms2_fgm_b_gsm_srvy_l2_bvec", "thd_fgs_gsm"])
+end
 
 end
